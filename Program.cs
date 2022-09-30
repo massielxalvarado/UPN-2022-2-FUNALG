40b18ca2160532783883a1ce3333c14b0d3dd743
massielxalvarado <114704811+massielxalvarado@users.noreply.github.com>
2022-09-30 13:42:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿char caracter;
+caracter = 'a';
+caracter = 'e';
+caracter = 'i';
+caracter = 'o';
+caracter = 'u';
+
+do
+{
+    Console.Write("INGRESE UN NUMERO SEGUID0 DE ENTER: ");
+    caracter = char.Parse(Console.ReadLine());
+}
+
+while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u');
