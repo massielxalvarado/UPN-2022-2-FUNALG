64e541a4c7d22e50e6ba65bae9b3f315f6455599
massielxalvarado <114704811+massielxalvarado@users.noreply.github.com>
2022-09-30 13:28:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int dividendo = 2;
+int divisor = 1;
+int divisiones = 0;
+
+while (dividendo <= 100)
+{
+    while (dividendo <= 100)
+    {
+        while (divisor <= dividendo)
+        {
+            if (dividendo % divisor == 0)
+            {
+                divisiones++;
+            }
+            divisor++;
+        }
+
+        if (divisiones == 2)
+        {
+            Console.WriteLine(dividendo);
+        }
+
+        dividendo++;
+        divisor = 1;
+        divisiones = 0;
+    }
+}
