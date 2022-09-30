c22a6532207780bc04f9c728ca12aacea2fe612d
massielxalvarado <114704811+massielxalvarado@users.noreply.github.com>
2022-09-30 13:41:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numero;
+do
+{
+    Console.Write("INGRESE UN NUMERO SEGUID0 DE ENTER: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+while (numero <= 100);
+
+Console.Write("ERROR!!! NUMERO FUERA DE RANGO: ");
