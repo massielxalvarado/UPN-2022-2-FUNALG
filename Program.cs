90f73d0b9dd6cab7575676da026457a563eba1bb
massielxalvarado <114704811+massielxalvarado@users.noreply.github.com>
2022-09-30 13:40:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int num, suma = 0;
+
+Console.WriteLine("ESCRIBA UN NUMERO: ");
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i = i + 2)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine("LA SUMA DE NUMEROS IMPARES ES : " + suma);
+Console.ReadKey();
