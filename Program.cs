e1186aa21b6b10959237520926042cb44d82a991
massielxalvarado <114704811+massielxalvarado@users.noreply.github.com>
2022-09-30 13:36:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int f, c;
+string n;
+
+Console.Write("Ingrese numero de filas: ");
+f = int.Parse(Console.ReadLine());
+Console.Write("Ingrese numero de columnas: ");
+c = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el numero a mostar: ");
+n = (Console.ReadLine());
+
+for (int i = 0; i < c; i++)
+{
+    for (int j = 0; j < f; j++)
+    {
+        Console.Write("" + n + "");
+    }
+    Console.Write("\n");
+}
